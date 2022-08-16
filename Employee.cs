f4f8e9e68d92abff5b78c6d42dc16e7fae2ccf19
Adarshamh <82590411+Adarshamh@users.noreply.github.com>
2022-08-16 15:44:30 +0530

    Add project files.

---
+++ Employee.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpemployee
+{
+    public class Employee
+    {
+
+        //created 2 variable one is string and othe one int type
+        //making it as null value by defualt
+
+
+        string name = string.Empty;
+        string department = string.Empty;
+        //Nullable<int> EmployeepId = null;
+
+        //end of variables
+
+        //here we use the above values and created property injection 
+        ///////////////   property injection   ////////////////
+        public string employeename { get; set; }
+        public string employeedepartment { get; set; }
+        
+      ////////////////  End of property injection   //////////////////
+        
+
+        
+
+
+        // lets create the constructors
+        //creating a constructor with no parameter
+        public Employee()
+        {
+            //an empty constuctor we didn't pass any parameters here
+        }
+
+        // constructor with ome parameter
+        public Employee(string empname)
+        {
+            name = empname;
+        }
+
+        //constructor with two parameter
+        //one is "string" another one is "int" 
+        public Employee(string empname,string empdep)
+        {
+            name = empname;
+            department = empdep;
+        }
+        /////////////////////////////////////////////   END OF CONSTRUCTORS   ////////////////////////////////////////////////////
+        
+        // lets create some methods here
+
+        //Here we created the show() method to print the value along with naming
+        public void show()
+        {
+            Console.WriteLine( "Employee Name:-> " +name + "Employee Department:-> " + department );
+        }
+
+          /// Here we concatinating the values
+          ///Only returns string value but the employee id is int
+        public string showcasevalue()
+        {
+            string showvalue = name + "" + department;
+            return showvalue;
+        }
+
+       /*public int showcasevalue()
+        {
+            int showup = 
+        }*/
+
+       
+        /// we reused the above methods for display the values of Property injection
+        public void show(string employeename, string employeedepartment)
+        {
+            Console.WriteLine("Employee Name:->" + employeename + "Employee Department:->" + employeedepartment);
+        }
+
+        public string showcasevalue(string employeename, string employeedepartment)
+        {
+            string showvalue = employeename + "" + employeedepartment;
+            return showvalue;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿
+/*CSharpemployee.Employee obj = new CSharpemployee.Employee();
+obj.show();
+string val = obj.showcasevalue();
+Console.WriteLine(val);
+
+
+CSharpemployee.Employee obj1 = new CSharpemployee.Employee("Abhishek");
+obj.show();
+string val1 = obj1.showcasevalue();
+Console.WriteLine(val1);
+
+
+CSharpemployee.Employee obj2 = new CSharpemployee.Employee("Abhishek",2);
+obj.show();
+string val2 = obj2.showcasevalue();
+Console.WriteLine(val2);*/
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//here we are using "using" keyword with namespace for future use to simplify the constructor declaration
+using CSharpemployee;
+
+
+/////////////without using namespace
+//declare without passing any parameters
+Employee obj = new Employee();
+obj.show();
+string val = obj.showcasevalue();
+Console.WriteLine(val);
+
+
+/////////////without using namespace
+//declare without passing any parameters
+Employee obj1 = new Employee("Abhishek");      //here we passed the values inside the constructor so it is known as constructor injection
+obj1.show();
+string val1 = obj1.showcasevalue();
+Console.WriteLine(val1);
+
+
+/////////////without using namespace
+//declare without passing any parameters
+Employee obj2 = new Employee("Abhishek","IT");
+obj2.show();
+string val2 = obj2.showcasevalue();
+Console.WriteLine(val2);
+
+//////////////////////////////////////////////// End of constructor injection //////////////////////////////////////////
+
+Employee obj3 = new Employee();
+/////////// property injection ////////////////
+obj3.employeename = "Adarsh";
+obj3.employeedepartment = "dev";
+/////////  end of property injection //////////
+obj3.show();
+string val3 = obj3.showcasevalue();
+Console.WriteLine(val3);
+
+
+
+//////////// Methdod injection ///////////
+Employee obj4 = new Employee();
+obj.show("Adarsh","Div");                           //here we injected the values inside the show and as well as in showcasevalue method
+string val4 = obj4.showcasevalue("Adarsh","Div");
+Console.WriteLine(val4);
+/////////// Methdo injection ends up here ///
